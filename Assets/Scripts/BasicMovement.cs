d8caed3530f2d15feb60bb477d986741c8f2b406
Vachri <vachri.attala@gmail.com>
2021-06-02 02:59:06 +0000

    No Comments

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move() { 
+    float x = Input.GetAxisRaw("Horizontal"); 
+    float moveBy = x * speed; 
+    rb.velocity = new Vector2(moveBy, rb.velocity.y); 
+}
+}
+
