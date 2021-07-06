f3354f08c07dfe95e76022422fdb37e20b570b4d
Vachri <vachri.attala@gmail.com>
2021-07-06 14:03:44 +0000

    No Comments

--- Assets/LevelController.cs
+++ Assets/LevelController.cs
@@ -2,15 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LevelController : MonoBehaviour
 {
     private Button l1, l2, l3;
+    private TextMeshProUGUI t1, t2, t3;
+    public Color dColor;
+
     void Start()
     {
         l1 = GameObject.Find("Level 1").GetComponent<Button>();
         l2 = GameObject.Find("Level 2").GetComponent<Button>();
         l3 = GameObject.Find("Level 3").GetComponent<Button>();
+
+        t1 = GameObject.Find("T1").GetComponent<TextMeshProUGUI>();
+        t2 = GameObject.Find("T2").GetComponent<TextMeshProUGUI>();
+        t3 = GameObject.Find("T3").GetComponent<TextMeshProUGUI>();
+
+        if(GameManager.Instance.LevelCompleted == 0){
+            l2.interactable = false;
+            l3.interactable = false;
+            t2.color = dColor;
+            t3.color = dColor;
+        }else if(GameManager.Instance.LevelCompleted == 1){
+            l3.interactable = false;
+            t3.color = dColor;
+        }
     }
 
     // Update is called once per frame
@@ -20,12 +38,15 @@ public class LevelController : MonoBehaviour
     }
 
     public void LoadLv1(){
+        AudioManager.playSound("ui_select");
         Loader.Load(Loader.Scene.Level1_Sewer);
     }
     public void LoadLv2(){
+        AudioManager.playSound("ui_select");
         Loader.Load(Loader.Scene.Level2_Slums);
     }
     public void LoadLv3(){
+        AudioManager.playSound("ui_select");
         Loader.Load(Loader.Scene.Level3_Suburbs);
     }
 }
