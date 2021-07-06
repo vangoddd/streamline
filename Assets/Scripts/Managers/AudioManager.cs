3730a1453bff9080ed57483b78e1ec13d33104b0
Vachri <vachri.attala@gmail.com>
2021-07-06 12:25:43 +0000

    Kabeh SFX wis melbu gan

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -7,21 +7,43 @@ public static class AudioManager
     private static GameObject oneShotGameObject;
     private static AudioSource audioSource;
 
+    private static GameObject BGM;
+    private static AudioSource bgmSource;
+
     public static void playSound(string playSound){
         if(oneShotGameObject == null){
             oneShotGameObject = new GameObject("OneshotSound");
             audioSource = oneShotGameObject.AddComponent<AudioSource>();
-            //audioSource.volume = 0.1f;
-            Debug.Log(ResourceManager.Instance.SFX);
+
             audioSource.outputAudioMixerGroup = ResourceManager.Instance.SFX;
             oneShotGameObject.AddComponent<DontDestroy>();
         }
         audioSource.PlayOneShot(ResourceManager.Instance.sound[playSound]);
     }
 
+    public static void playMusic(string bgMusic){
+        if(BGM != null){
+            Object.Destroy(BGM);
+        }
+        BGM = new GameObject("BGM");
+        bgmSource = BGM.AddComponent<AudioSource>();
+        bgmSource.outputAudioMixerGroup = ResourceManager.Instance.music;
+        BGM.AddComponent<DontDestroy>();
+        bgmSource.clip = ResourceManager.Instance.sound[bgMusic];
+        bgmSource.Play();
+    }
+
+    // **********************************************************************************************//
+
     public static void changeSFXVol(){
-        //Debug.Log(GameManager.Instance.sfxVolume);
-        //Debug.Log(audioSource);
         audioSource.outputAudioMixerGroup.audioMixer.SetFloat("sfx", GameManager.Instance.sfxVolume);
     }
+    public static void changeMusicVol(){
+        audioSource.outputAudioMixerGroup.audioMixer.SetFloat("music", GameManager.Instance.musicVolume);
+    }
+    public static void changeMasterVol(){
+        audioSource.outputAudioMixerGroup.audioMixer.SetFloat("master", GameManager.Instance.masterVolume);
+    }
+
+    
 }
